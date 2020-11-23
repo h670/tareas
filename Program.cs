bde811c4184025eb834697b3fcbf062516d59741
h670 <62627559+h670@users.noreply.github.com>
2020-11-23 09:02:16 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = 0;
+            string bin = "";
+            Console.WriteLine("Escriba su numero:");
+            n = int.Parse(Console.ReadLine());
+
+            while (true){
+                if ((n % 2) != 0)
+                    bin = "1" + bin;
+                else
+                    bin = "0" + bin;
+                n /= 2;
+                if (n <= 0)
+                    break;
+
+
+            }
+            Console.WriteLine("Su resultado es:" + bin);
+            Console.ReadKey();
+        }
+    }
+}
